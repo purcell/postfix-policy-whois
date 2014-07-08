363be6fb6c6f0f2d187da2d7e62f57d233d6613f
Steve Purcell <steve@sanityinc.com>
2014-07-08 10:21:44 +0100

    Extract blacklist policy into separate module

--- src/Main.hs
+++ src/Main.hs
@@ -6,35 +6,11 @@ TODO: command line args
 TODO: keep patterns in a file
 -}
 module Main where
-import           Control.Applicative    ((*>))
-import           Control.Monad.Except
-import           Control.Monad.Identity
-import qualified Data.ByteString.Char8  as B8
-import qualified Data.Map               as Map
-import           Data.Maybe             (mapMaybe)
-import           Lookups
+import qualified Data.ByteString.Char8 as B8
 import           PostfixPolicy
-import           System.Environment     (getArgs)
-import           Text.Parsec.Char
-import           Text.Parsec.Prim
-import qualified Text.Regex.PCRE.Light  as PCRE
-
-domainFromEmail :: String -> ExceptT String Identity Domain
-domainFromEmail email = withExcept translateError . ExceptT . Identity $ parseEmail email
-  where
-    translateError _ = "Invalid email address: " ++ show email
-    parseEmail = fmap B8.pack . parse (notAt *> char '@' *> notAt) ""
-    notAt = many1 $ noneOf "@"
-    many1 p = liftM2 (:) p (many p)
-
-
-senderDomain :: PolicyInfo -> ExceptT String Identity Domain
-senderDomain info = do
-  email <- case Map.lookup "sender" info of
-             Just s -> return s
-             Nothing -> throwError "No sender provided"
-  domainFromEmail email
-
+import           System.Environment    (getArgs)
+import qualified Text.Regex.PCRE.Light as PCRE
+import           WhoisPolicy
 
 patterns :: Either String [PCRE.Regex]
 patterns = mapM compile [
@@ -47,35 +23,9 @@ patterns = mapM compile [
   where
     compile s = PCRE.compileM (B8.pack s) [PCRE.caseless]
 
-
-decideBasedOnWhois :: [PCRE.Regex] -> WhoisInfo -> IO Decision
-decideBasedOnWhois matchers (WhoisInfo info) =
-  let binfo = B8.pack info in
-  if null $ mapMaybe (\p -> PCRE.match p binfo []) matchers
-  then return $ Decision Dunno
-  else do
-    putStrLn "Rejecting based on pattern match"
-    return $ Decision Reject
-
-
-blacklistPolicy :: [PCRE.Regex] -> Policy
-blacklistPolicy matchers info = do
-  result <- runExceptT lookupAndDecide
-  case result of
-    Left e -> do
-      putStrLn e
-      return $ Decision Dunno
-    Right d -> return d
-  where
-    lookupAndDecide = do
-      domain <- mapExceptT (return . runIdentity) $ senderDomain info
-      winfo <- ExceptT $ whois domain
-      liftIO $ decideBasedOnWhois matchers winfo
-
-
 main :: IO ()
 main = do
   [port] <- getArgs
   case patterns of
     Left e -> putStrLn e
-    Right ps -> serveTCP (read port :: Int) putStrLn (blacklistPolicy ps)
+    Right ps -> serveTCP (read port :: Int) putStrLn (whoisBlacklistPolicy ps)
---
+++ src/WhoisPolicy.hs
@@ -0,0 +1,56 @@
+module WhoisPolicy
+       ( whoisBlacklistPolicy
+       )
+where
+import           Control.Applicative    ((*>))
+import           Control.Monad.Except
+import           Control.Monad.Identity
+import qualified Data.ByteString.Char8  as B8
+import qualified Data.Map               as Map
+import           Data.Maybe             (mapMaybe)
+import           Lookups
+import           PostfixPolicy
+import           Text.Parsec.Char
+import           Text.Parsec.Prim
+import qualified Text.Regex.PCRE.Light  as PCRE
+
+domainFromEmail :: String -> ExceptT String Identity Domain
+domainFromEmail email = withExcept translateError . ExceptT . Identity $ parseEmail email
+  where
+    translateError _ = "Invalid email address: " ++ show email
+    parseEmail = fmap B8.pack . parse (notAt *> char '@' *> notAt) ""
+    notAt = many1 $ noneOf "@"
+    many1 p = liftM2 (:) p (many p)
+
+
+senderDomain :: PolicyInfo -> ExceptT String Identity Domain
+senderDomain info = do
+  email <- case Map.lookup "sender" info of
+             Just s -> return s
+             Nothing -> throwError "No sender provided"
+  domainFromEmail email
+
+
+decideBasedOnWhois :: [PCRE.Regex] -> WhoisInfo -> IO Decision
+decideBasedOnWhois matchers (WhoisInfo info) =
+  let binfo = B8.pack info in
+  if null $ mapMaybe (\p -> PCRE.match p binfo []) matchers
+  then return $ Decision Dunno
+  else do
+    putStrLn "Rejecting based on pattern match"
+    return $ Decision Reject
+
+
+whoisBlacklistPolicy :: [PCRE.Regex] -> Policy
+whoisBlacklistPolicy matchers info = do
+  result <- runExceptT lookupAndDecide
+  case result of
+    Left e -> do
+      putStrLn e
+      return $ Decision Dunno
+    Right d -> return d
+  where
+    lookupAndDecide = do
+      domain <- mapExceptT (return . runIdentity) $ senderDomain info
+      winfo <- ExceptT $ whois domain
+      liftIO $ decideBasedOnWhois matchers winfo
